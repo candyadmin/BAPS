9e52dc469e09676186261c95d06c2cabd7491c66
BaseAccept <193661405@qq.com>
2018-04-13 14:13:23 +0800

    成立项目

---
+++ chat/src/main/java/tk/baseaccept/pass/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package tk.baseaccept.pass.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class ChatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+}
